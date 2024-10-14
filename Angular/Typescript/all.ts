dec182741e8aa70aad128e4cbbb2c0694749254f
Sreeramofficial <mssreerammanatt@gmail.com>
2024-10-15 00:49:37 +0530

    custom types

--- Angular/Typescript/all.ts
+++ Angular/Typescript/all.ts
@@ -1,18 +1,6 @@
-type values={
-    name:string,
-    age:number,
-    isMarried?:boolean
-}
+//custom types
 
+type Gender="male" | "female";
 
-let student={
-    name:"sreeram",
-    age:20,
-    isMarries:true
-}
-function getUserName(det:values,det2:values):string|number{
-    return det.age;
-
-}
-
-const retuenvAL=getUserName(student,student);
+// here we can use only either male or female
+let gender:Gender="male";
