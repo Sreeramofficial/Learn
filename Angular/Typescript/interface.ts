ad324c888e7e386175846e2658dcced77678be7d
Sreeramofficial <mssreerammanatt@gmail.com>
2024-10-15 00:23:15 +0530

    type like interfae

--- Angular/Typescript/interface.ts
+++ Angular/Typescript/interface.ts
@@ -1,12 +1,19 @@
-interface  Details{
-    name:string;
-    age:Number;
-    getName: ()=>void;
+// interface  Details{
+//     name:string;
+//     age:Number;
+//     getName: ()=>void;
+// }
+
+
+type Details= {
+  name: string;
+  age: Number;
+  getName: () => void;
 }
 
 //like class in java
 //define variables use in declaration
-
+// type same as inteface diff is "="
 let AdminDetails : Details={
    name: "sreeram",
     age:20,
