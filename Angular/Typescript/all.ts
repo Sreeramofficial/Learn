da1478f4bd3b12094d0ef4bb7169b5fd1b4715a4
Sreeramofficial <mssreerammanatt@gmail.com>
2024-10-15 00:45:18 +0530

    functions

--- Angular/Typescript/all.ts
+++ Angular/Typescript/all.ts
@@ -4,10 +4,15 @@ type values={
     isMarried?:boolean
 }
 
-let Sreream:values={
+
+let student={
     name:"sreeram",
     age:20,
-//    isMarried: true
+    isMarries:true
+}
+function getUserName(det:values,det2:values):string|number{
+    return det.age;
+
 }
 
-//
+const retuenvAL=getUserName(student,student);
