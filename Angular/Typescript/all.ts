62c704c245115d145c22fc3e62e2e76843623ad0
Sreeramofficial <mssreerammanatt@gmail.com>
2024-10-15 01:42:58 +0530

    pick omit requeired

--- Angular/Typescript/all.ts
+++ Angular/Typescript/all.ts
@@ -1,29 +1,39 @@
-// key of values of
+// utility type
+
+type Users = {
+  //    readonly name:string;    we can make thisas read only
+   name: string;
+  age: number;
+};
 
 
-// this is object
-const Status={
-    FAIL:"fail",
-    PASS:"pass"
+// details are read on;y
+const usDetails:Readonly<Users>={
+    name:"sreeram",
+    age:20
 }
 
-// so we need to convert to type format
-// then we are taking it as key
-function getStatus(id :string,status: keyof typeof Status){
-    console.log(Status[status]);
-}
+
+// varianles are optiosnal
+const usDetails2: Partial<Users> = {
+//   name: "sreeram",
+//   age: 20,
+};
+
+// we need to pass all the vaels irrespective to optional
+const usDetails3: Required<Users> = {
+    name: "sreeram",
+    age: 20,
+};
 
 
-
-// this will generate
-
-// type status={
-//     fail:string;
-//     pass:string;
-// }
-
-
-// interface Person {
-//   name: string;
-//   age: number;
-// }
+// here we can t add name becouse it excpeig age only but that is mandotaory
+const usDetails4: Pick<Users,"age"> = {
+    // name: "sreeram",
+    age: 20,
+};
+// here we can t add age becouse it excpeig name and age will omit only but that is mandotaory
+const usDetails5: Omit<Users,"age"> = {
+    name: "sreeram",
+    // age: 20,
+};
