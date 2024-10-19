66c3867f5569aa3db524672796482dee26962a71
Sreeramofficial <mssreerammanatt@gmail.com>
2024-10-19 19:03:09 +0530

    todo all most done

--- Angular/Angular/wishList/src/app/app.component.ts
+++ Angular/Angular/wishList/src/app/app.component.ts
@@ -2,19 +2,32 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { wishItem } from '../shared/modules/wishlist';
 import { CommonModule } from '@angular/common'; // Import CommonModule
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [CommonModule, RouterOutlet], // Add CommonModule and RouterOutlet here
+  imports: [CommonModule, RouterOutlet, FormsModule], // Add CommonModule and RouterOutlet here
 })
 export class AppComponent {
+  newTarget = '';
+  newTargetStatus: boolean = false;
+
   title = 'Sreeram';
   items: wishItem[] = [
-    new wishItem('angular', true),
-    new wishItem('move to another company', false),
-    new wishItem('be settle', false),
+    // new wishItem('angular', true),
+    // new wishItem('move to another company', false),
+    // new wishItem('be settle', false),
   ];
+  toggleItem(event: wishItem) {
+    event.isComplete = !event.isComplete;
+  }
+  addWish() {
+    this.items.push(new wishItem(this.newTarget, this.newTargetStatus));
+    this.newTarget = '';
+    this.newTargetStatus = false;
+  }
+  account = 'Sreeram';
 }
